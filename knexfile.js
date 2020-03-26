bb04a1a383ad27730cf38ba13bd8b9905335509c
Josiah <reikiryo@yahoo.com>
2020-03-26 13:37:36 -0400

    updated production to use pg

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,18 @@ module.exports = {
       directory: "./data/seeds",
     },
   },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
   testing: {
     client: "sqlite3",
     connection: {
